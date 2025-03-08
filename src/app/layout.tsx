d1b085ac2ddd19f2861d586d025f3cf182117cfe
Aman Anil <amananiloffical@gmail.com>
2025-03-08 15:52:47 +0530

    30% Pending

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import RootLayoutWrapper from "./Components/layouts/RootLayoutWrapper";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <RootLayoutWrapper>
           {children}
+          <Analytics />
         </RootLayoutWrapper>
       </body>
     </html>
